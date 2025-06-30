1bd0ca76fc859c26600cc57a5b7fcded6ea6ff4b
Ajay Velanchery <ajayvelanchery@gmail.com>
2025-06-30 16:42:15 +0530

    June_30_task

--- Task/src/Employee.java
+++ Task/src/Employee.java
@@ -49,36 +49,4 @@ public class Employee {
         this.department = department;
     }
 
-    interface BonusCalulater{
-        double calculate(double Salary);
-    }
-
-    public static void main(String[] args) {
-
-
-    List<Employee> employees = new ArrayList<Employee>();
-        employees.add(new Employee("Ajay", 200000, LocalDate.of(2020, 9, 15), Optional.of("Civil")));
-        employees.add(new Employee("Suresh", 300000, LocalDate.of(2023, 11, 16), Optional.of("IT")));
-        employees.add(new Employee("Vijesh", 400000, LocalDate.of(2002, 12, 17), Optional.of("Defence")));
-        employees.add(new Employee("Gukesh", 600000, LocalDate.of(2003, 1, 17), Optional.of("AirForce")));
-        employees.add(new Employee("Ramesh", 600000, LocalDate.of(2019, 4, 18), Optional.of("Cricket")));
-
-    BonusCalulater bonusCalulater=(salary)->(salary*0.10);
-
-     employees.forEach(emp-> System.out.println(emp.getName()+ " "+emp.getSalary()));
-//        Filter employees who joined after 2020
-     employees.stream().filter(emp->emp.getJoiningDate().isAfter(LocalDate.of(2020,2,2)))
-             .forEach(emp-> System.out.println(emp.getName()));
-
-
-//        - Map employee names to uppercase
-     employees.stream()
-             .map(emp ->emp.getName().toUpperCase() )
-             .forEach(System.out::println);
-
-//    - Count how many employees have a salary greater than ₹50,000
-
-        long Salaryhigher=employees.stream().filter(emp->emp.getSalary()>50000).count();
-
-        System.out.println(Salaryhigher);
-}}
+   }
